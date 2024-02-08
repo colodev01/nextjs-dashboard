8a37bec5a0bbab6654a9051752b6679741dec8db
colodev01 <francomuzzin01@gmail.com>
2024-02-08 09:54:59 -0300

    Getting Started Part 2

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,9 +10,6 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${montserrat.className} antialiased`}>
         {children}
-        <footer className="flex items-center justify-center py-10">
-          Hecho con amor por la gente de Vercel
-        </footer>
       </body>
     </html>
   );
